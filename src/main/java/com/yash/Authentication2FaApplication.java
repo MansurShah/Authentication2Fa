7f5ad47d20903e94c08ae62b65f3bc982495572b
mansur <mansur.shahahpurkar@yash.com>
2020-01-08 14:59:23 +0530

    initialized project

---
+++ src/main/java/com/yash/Authentication2FaApplication.java
@@ -0,0 +1,13 @@
+package com.yash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authentication2FaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Authentication2FaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/Authentication2FaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Authentication2FaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
